2c3aecf51531fadd9271ccc5fd8adfe3ffdefe36
Eric Walters <ejwalters24@gmail.com>
2024-03-20 21:18:48 -0400

    added connection to firestore

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const admin = require('firebase-admin');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,6 +11,32 @@ app.use(express.json());
 
 // Example route
 app.get('/', (req, res) => res.send('Hello World!'));
+
+var serviceAccount = require('./config/serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+app.get('/fetchSampleCollection', async (req, res) => {
+    console.log('Fetching collection');
+    try {
+        const sampleCollectionRef = db.collection('sampleCollection');
+        const snapshot = await sampleCollectionRef.get();
+        const documents = [];
+        snapshot.forEach(doc => {
+            documents.push({ id: doc.id, ...doc.data() });
+        });
+        res.status(200).json(documents);
+        console.log(documents);
+    } catch (error) {
+        console.error('Error fetching collection:', error);
+        res.status(500).send('Error fetching collection');
+    }
+});
+
 
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Connection successful!' });
